745b7b3b0c59c8afaaf280e07efaf5a3bb9a748c
saimanohar9966 <reddy.saimanu123@gmail.com>
2022-12-02 12:38:56 +0530

    create alb ec2

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"                               # Mention the required region
-  access_key = "AKIA6KZCXLPISR4LLHPQ"                     # Mention the access key in place
-  secret_key = "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" # Mention the secret key in place
+  access_key = ""                     # Mention the access key in place
+  secret_key = "" # Mention the secret key in place
 }
